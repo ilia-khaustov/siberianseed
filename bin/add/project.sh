8484a03065f82509e70c1681862d681ca2b67a44
Ilya Khaustov <ilya.khaustov@gmail.com>
2015-10-24 15:12:43 +0700

    project adding improved, seed modified to check if shortcut exists

--- bin/add/project.sh
+++ bin/add/project.sh
@@ -3,27 +3,32 @@
 # Creates a new seed-project
 
 echo "Creating seed-project in $userpwd";
-cd $userpwd;
+cd "$userpwd";
 
-read -p "Project directory: " newdir
-if [ -z $newdir ];
+read -p "Project full name: " newdir
+if [ -z "$newdir" ];
   then
-    echo "Empty directory given, exit"; exit 0; fi
-if [ -d $newdir ];
+    echo "Empty name given, exit"; exit 0; fi
+if [ -d "$newdir" ];
   then
     echo "Directory $newdir already exists";
     exit 0;
 fi
 
 read -p "Project shortcut: " newcmd
-if [ -z $newcmd ];
+if [ -z "$newcmd" ];
   then
     echo "Empty shortcut given, exit"; exit 0; fi
 
-git clone git@github.com:ilya-khaustov/siberianseed.git $newdir;
-cd $newdir;
-sudo ./seed --new $newcmd --owner $(whoami);
+git clone git@github.com:ilya-khaustov/siberianseed.git "$newdir";
+cd "$newdir";
+sudo ./seed --new "$newcmd" --owner $(whoami);
 rm -rf .git;
 mv README.md SIBERIANSEED.md;
 rm LICENSE.md;
+touch README.md;
+echo "# $newdir\n\n" >> README.md;
+echo "## Install\n" >> README.md;
+echo " 1. \`sudo ./seed --new $newcmd --owner \$(whoami)\`" >> README.md;
+echo " 1. \`$newcmd install\`" >> README.md;
 echo "Project $newdir created successfully";
